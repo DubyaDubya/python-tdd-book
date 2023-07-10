f9c8438d000e31189e335a89850d75fc4b63caca
William Weiler <williamweilercash@gmail.com>
2023-07-10 13:57:01 -0500

    Front page html now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -40,13 +40,14 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "new to-do item did not appear in table"
         )
 
         # There is still a text box inviting her to add another item. She enters
         # "Use peacock feathers to make a fly"
         self.fail('Finish the test!')
-        
+
         # the page updates again, and now shows both of the items on her list
 
         # Edith wonders whether the site will remember her list.
